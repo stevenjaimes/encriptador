fda4f14ccda18cfa32e84b384468263f38c8c7c8
Steven <hensteve250@gmail.com>
2024-08-10 00:21:16 -0500

    Proyecto final

---
+++ README.md
@@ -0,0 +1,76 @@
+🛡️ Encriptado: Tu herramienta de Encriptación personal
+
+
+📜 Descripción
+
+Encriptado es una herramienta simple pero poderosa para encriptar y desencriptar mensajes, diseñada para proteger tu información confidencial. Con una interfaz amigable y un alto nivel de seguridad, SecureCrypt es la solución perfecta para quienes buscan una manera confiable de mantener su información a salvo.
+
+
+🚀 Características
+Encriptación segura utilizando algoritmos robustos.
+Desencriptación fácil para recuperar tus mensajes.
+Interfaz amigable y fácil de usar.
+Soporte para múltiples formatos de entrada (texto plano, archivos).
+Compatibilidad cross-platform (Windows, Mac, Linux).
+
+
+🛠️ Tecnologías Utilizadas
+Lenguaje: Javascript
+Estilos: CSS
+Versionado: Git
+
+
+🔧 Instalación
+Clona el repositorio:
+
+git clone https://github.com/stevenjaimes/encriptador.git
+cd tu_proyecto
+
+
+Ejecuta la aplicación:
+
+Plugin: Go Live
+
+
+📷 Capturas de Pantalla
+¡Mira cómo luce! 👇
+
+<p align="center">
+  <img src="/assets/pantallazo1.png" alt="Seguridad encriptado en acción" width="400"/>
+  <img src="/assets/pantallazo2.png" alt="Interfaz Seguridad Encriptado" width="400"/>
+</p>
+
+
+💻 Uso
+Encriptar un mensaje:
+
+Escribe o carga tu mensaje en el campo de texto.
+Haz clic en "Encriptar".
+¡Listo! Tu mensaje ahora está seguro.
+
+Desencriptar un mensaje:
+
+Copia el mensaje encriptado.
+Pegalo en la caja de texto
+Da click en "Desencriptar" para ver el mensaje original.
+
+
+🧑‍💻 Contribución
+Las contribuciones son bienvenidas. Siéntete libre de abrir un issue o enviar un pull request.
+
+Haz un fork del proyecto.
+Crea una nueva rama (git checkout -b mi-nueva-rama).
+Realiza tus cambios.
+Envía un pull request.
+
+
+📄 Licencia
+Este proyecto está bajo la licencia MIT. Consulta el archivo LICENSE para más detalles.
+
+
+🌐 Contacto
+Nombre: Henry Steeven Jaimes Bastos
+Correo: hensteve250@gmail.com
+LinkedIn: linkedin.com/in/steven-jaimes-0767a2205/
+GitHub: github.com/stevenjaimes
+
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const resultDiv = document.getElementById("result");
 
   // Función para encriptar
-  function encrypt(text) {
+  function encriptar(text) {
     return text
       .replace(/e/g, "enter")
       .replace(/i/g, "imes")
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Función para desencriptar
-  function decrypt(text) {
+  function desencriptar(text) {
     return text
       .replace(/enter/g, "e")
       .replace(/imes/g, "i")
@@ -27,36 +27,35 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // Evento para el botón Encriptar
-  encriptarButton.addEventListener("click",  (e) => {
+  encriptarButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const textareaValue = document.getElementById("textarea").value;
-    const encryptedText = encrypt(textareaValue);
-    showResult(encryptedText);
+    const textareaValor = document.getElementById("textarea").value;
+    const textoEncriptado = encriptar(textareaValor);
+    mostrarResultado(textoEncriptado);
   });
 
   // Evento para el botón Desencriptar
   desencriptarButton.addEventListener("click", (e) => {
     e.preventDefault();
     const textareaValue = document.getElementById("textarea").value;
-    const decryptedText = decrypt(textareaValue);
-    showResult(decryptedText);
+    const textoDesencriptado = desencriptar(textareaValue);
+    mostrarResultado(textoDesencriptado);
   });
 
   // Función para mostrar el resultado y cambiar la vista
-  function showResult(text) {
+  function mostrarResultado(text) {
     resultDiv.textContent = text;
     initialFlexDiv.style.display = "none";
     newFlexDiv.style.display = "flex";
   }
 
-   // Función para mostrar el toast
-    // Función para mostrar el toast
-    function showToast(message) {
-      const notifications = document.querySelector('.notifications');
-      const toast = document.createElement('div');
-      toast.className = 'toast';
+  // Función para mostrar el toast
+  function mostrarToast(message) {
+    const notifications = document.querySelector(".notifications");
+    const toast = document.createElement("div");
+    toast.className = "toast";
 
-      toast.innerHTML = `
+    toast.innerHTML = `
           <i class="fa-solid fa-circle-check"></i>
           <div class="content">
               <div class="title">Copiado</div>
@@ -65,34 +64,34 @@ document.addEventListener("DOMContentLoaded", () => {
           <i class="fa-solid fa-xmark" onclick="(this.parentElement).remove()"></i>
       `;
 
-      notifications.appendChild(toast);
+    notifications.appendChild(toast);
 
+    setTimeout(() => {
+      toast.classList.add("show");
+    }, 10);
+
+    // Ocultar el toast después de 1.5 segundos
+    setTimeout(() => {
+      toast.classList.remove("show");
+      toast.classList.add("hide");
+
+      // Eliminar el toast después de la animación
       setTimeout(() => {
-          toast.classList.add("show");
-      }, 10);
-
-      // Ocultar el toast después de 1.5 segundos
-      setTimeout(() => {
-          toast.classList.remove("show");
-          toast.classList.add("hide");
-
-          // Eliminar el toast después de la animación
-          setTimeout(() => {
-              toast.remove();
-          }, 500);
-      }, 1500);
+        toast.remove();
+      }, 500);
+    }, 1500);
   }
   // Funcionalidad del botón copiar
   const copyButton = document.getElementById("copyButton");
   copyButton.addEventListener("click", function () {
     const textToCopy = resultDiv.textContent;
     navigator.clipboard.writeText(textToCopy).then(
-        function () {
-            showToast("Texto copiado al portapapeles");
-        },
-        function (err) {
-            console.error("Error al copiar el texto: ", err);
-        }
+      function () {
+        mostrarToast("Texto copiado al portapapeles");
+      },
+      function (err) {
+        console.error("Error al copiar el texto: ", err);
+      }
     );
-});
+  });
 });
